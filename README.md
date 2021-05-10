db2112176598f554bc80935c8ec8d171798d7ee7
Cuda-Chen <clh960524@gmail.com>
2021-05-10 15:15:23 +0800

    finish wsgi demo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # python-wsgi-socket-demo
+Python socket and socket with WSGI demo.
+
+## References
+- https://www.reddit.com/r/learnpython/comments/1szfcf/what_exactly_is_wsgi/
+- https://docs.python.org/3/library/wsgiref.html#module-wsgiref.simple_server
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import socket
+from wsgiref import simple_server
 
-def serve():
+def serve(app):
     listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listener.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listener.bind(('', 8080))
@@ -11,12 +12,24 @@ def serve():
         print(f'request text: {request}')
         method, rest = request.decode().split(' ', 1)
         path, rest = rest.split(None, 1)
-        print(f'received request, method: {method}, path: {path}')
-        s.send(b'HTTP/0.9 200 OK\r\nContent-Type: text/plain')
-        s.send(b'\r\n\r\n')
-        string = f'You asked to {method} {path}'
-        s.send(string.encode())
+        def start_reponse(status, headers):
+            print('sending headers')
+            front_matter = '\r\n'.join(['HTTP/0.9 '+status] + [k+': '+v for k, v in headers])
+            s.send(front_matter.encode())
+            s.send(b'\r\n\r\n')
+        environ = {'REQUEST_METHOD': method, 'PATH_INFO': path}
+        for data in app(environ, start_reponse): # WSGI specifies that an iterator should be returned
+            print('sending data')
+            s.send(data)
         s.close()
 
+def wsgihandler(environ, start_reponse):
+    """WSGI application"""
+    start_reponse("200 OK", [('Content-Type','text/plain')])
+    #r = Request(environ)
+    return [('You asked to '+environ['REQUEST_METHOD']+' '+environ['PATH_INFO']).encode(), 
+            b' some more data',
+            b'\r\n']
+
 if __name__ == '__main__':
-    serve()
+    serve(wsgihandler)
