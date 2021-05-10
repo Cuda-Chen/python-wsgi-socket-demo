db6decdf39693e6e92eb759b6d71fff3c496b209
Cuda Chen <clh960524@gmail.com>
2021-05-10 22:59:08 +0800

    set decode() and encode() use utf-8
    Set decode() and encode() in wsgi.py and asgi.py as it is a good
    practice.

--- asgi.py
+++ asgi.py
@@ -11,12 +11,12 @@ def serve(app):
         s, addr = listener.accept()
         request = s.recv(10000)
         print(f'request text: {request}')
-        method, rest = request.decode().split(' ', 1)
+        method, rest = request.decode('utf-8').split(' ', 1)
         path, rest = rest.split(None, 1)
         def start_reponse(status, headers):
             print('sending headers')
             front_matter = '\r\n'.join(['HTTP/0.9 '+status] + [k+': '+v for k, v in headers])
-            s.send(front_matter.encode())
+            s.send(front_matter.encode('utf-8'))
             s.send(b'\r\n\r\n')
         environ = {'REQUEST_METHOD': method, 'PATH_INFO': path}
         for data in app(environ, start_reponse): # WSGI specifies that an iterator should be returned
@@ -28,7 +28,7 @@ def wsgihandler(environ, start_reponse):
     """WSGI application"""
     start_reponse("200 OK", [('Content-Type','text/plain')])
     #r = Request(environ)
-    return [('You asked to '+environ['REQUEST_METHOD']+' '+environ['PATH_INFO']).encode(), 
+    return [('You asked to '+environ['REQUEST_METHOD']+' '+environ['PATH_INFO']).encode('utf-8'), 
             b' some more data',
             b'\r\n']
 
